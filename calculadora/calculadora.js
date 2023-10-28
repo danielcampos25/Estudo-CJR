682b991418bcd5467e11fcc452beb12e5280b787
Daniel Campos <camposilvadaniel@gmail.com>
2023-10-28 16:43:22 -0300

    inicio da calculadora js

---
+++ calculadora/calculadora.js
@@ -0,0 +1,19 @@
+function Soma (num1,num2) {
+    return num1 + num2
+}
+
+function Subtracao (num1,num2){
+    return num1- num2
+}
+
+function Divisao (num1,num2){
+    return num1 / num2
+}
+
+function Multiplicacao (num1,num2){
+    return num1 * num2
+}
+
+function Exp (num1,num2){
+    return num1 ** num2
+}
